0e81b19ec4e253d60e06d79c1b605a484a2f37fe
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2025-05-26 13:02:28 +0200

    Add picture of myself to the HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -49,8 +49,13 @@ const HeroSection = () => {
 
           <div className="flex justify-center">
             <div className="w-80 h-80 bg-gradient-to-br from-blue-400 to-indigo-600 rounded-full flex items-center justify-center shadow-2xl">
-              <div className="w-72 h-72 bg-white rounded-full flex items-center justify-center">
-                <span className="text-6xl">👨‍🎓</span>
+              <div className="w-72 h-72 bg-white rounded-full flex items-center justify-center overflow-hidden">
+                {/* Remplace le lien ci-dessous par celui de ton image */}
+                <img
+                  src="https://scontent.fbru2-1.fna.fbcdn.net/v/t39.30808-6/294673146_824591818508047_5062163756966266510_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=qAW2JiGaCjkQ7kNvwHKssVi&_nc_oc=Adnaa4KKJjw1_Zs5Yt8cSTu2d0X9wWc2xYK91rXJmykL8mCxT4hnrUho73pp5k7LJVDq4a4nKkgTdZ35Rg5mfYI0&_nc_zt=23&_nc_ht=scontent.fbru2-1.fna&_nc_gid=-bvJVmj62BxChGz_TPfCYg&oh=00_AfJy93RBRzayzebGAhMsg2pQIAJNhGyPGrpCsQCP0_gHzg&oe=683A024B"
+                  alt="Portrait de HUYBRECHTS Louis"
+                  className="w-full h-full object-cover"
+                />
               </div>
             </div>
           </div>
